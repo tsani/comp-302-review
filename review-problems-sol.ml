43348b1e063667f3cc66288b23a1c07bdf783cf5
Jiahao <jiahao.chen@mail.mcgill.ca>
2019-12-01 15:33:35 -0500

    Add some other solutions
    Added fold_left' , map', rev', for_all, exists, for_all', exists', sum, combine, map2

--- review-problems-sol.ml
+++ review-problems-sol.ml
@@ -29,3 +29,43 @@ let rec fold_left f acc l = match l with
 
 let rec rev l = 
   fold_left (fun acc x-> Cons(x,acc)) Nil l
+
+let rec append l1 l2 = match l1 with
+| Nil -> l2
+| Cons(x,xs) -> Cons(x, (append xs l2))
+
+let rec fold_right f l acc = match l with
+  | Nil -> acc
+  | Cons(x,xs) -> f x (fold_right f xs acc)
+
+let fold_left' f e l = 
+	fold_right (fun a b -> f b a) (fold_right (fun a b -> append b (Cons(a, Nil))) l Nil)
+
+let map' f l = 
+  	fold_right (fun x acc -> Cons(f x, acc)) l Nil
+
+let rev' l =
+	fold_right (fun a b -> append b (Cons(a, Nil))) l Nil
+
+let for_all p l = 
+  	fold_right (fun x acc -> acc && p x) l true
+
+let exists p l =
+  	fold_right (fun x acc -> acc || p x) l false
+  
+let for_all' p l = 
+ 	not (exists (fun x -> not p) l)
+
+let exists' p l = 
+  	not (for_all (fun x -> not p) l)
+
+let sum l = 
+  	fold_right (+) l 0
+   
+let rec combine l1 l2 = match l1, l2 with 
+| Nil, Nil -> Nil
+| Cons(x,xs), Cons(y,ys) -> Cons((x,y), combine xs ys)
+
+let map2 f l1 l2 = 
+  	let tup = combine l1 l2 in
+  	map (fun (x,y) -> f x y) tup
