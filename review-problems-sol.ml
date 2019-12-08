cb3d029d1a69f99af51bd3f4feb7b791f202150f
Cunyuan Zhao <58645151+JackyMouse724@users.noreply.github.com>
2019-12-08 01:50:38 -0500

    My attempt at the Lazy module
    added my solutions to all problems in the Lazy module

--- review-problems-sol.ml
+++ review-problems-sol.ml
@@ -29,3 +29,82 @@ let rec fold_left f acc l = match l with
 
 let rec rev l = 
   fold_left (fun acc x-> Cons(x,acc)) Nil l
+  
+(* Lazy Programming *)
+module Lazy = struct
+  type 'a susp = Susp of (unit -> 'a)
+  let delay f = Susp f
+  let force (Susp f) = f ()
+
+  (* Usual definition of streams. *)
+  type 'a str =
+    { hd : 'a
+    ; tl : 'a str susp
+    }
+    
+  (* REMARK: I start from zero *)
+  let rec nth n s = 
+    if n = 0 then s.hd else nth (n-1) (force s.tl) 
+        
+  let rec fib : int str = 
+    let rec go n m =
+      {hd = n; 
+       tl = Susp (fun () -> go (n+m) n)}
+    in
+    go 1 0
+      
+  let rec seq f = 
+    let rec go n = 
+      {hd = f n;
+       tl = Susp (fun () -> go (n+1))}
+    in 
+    go 0
+
+  let wallis_1 =
+    let rec f n =
+      if n = 0 then 4.0 /. 3.0 
+      else 
+        let x = (float_of_int) (4*(n+1)*(n+1)) in
+        let y = (float_of_int) (4*(n+1)*(n+1) - 1) in
+        (x /. y) *. (f (n-1))
+    in
+    seq f   
+    
+  (* REMARK: index starts from 1 *)
+  let wallis_2 = 
+    let rec go n w_n = 
+      let x = (float_of_int) (4*(n+1)*(n+1)) in
+      let y = (float_of_int) (4*(n+1)*(n+1) - 1) in
+      let a_n = x /. y in
+      {
+        hd = w_n;
+        tl = Susp (fun () -> go (n+1) (a_n *. w_n))
+      }
+    in
+    go 1 (4.0 /. 3.0)
+      
+  let superc m n = 
+    let rec fact x = match x with
+      | 0 -> 1
+      | d -> d * (fact (d-1))
+    in 
+    (fact (2*m))*(fact (2*n)) / ((fact (m+n))*(fact m)*(fact n))
+                                
+  let supercatalan = 
+    let rec get_col m k = 
+      {
+        hd = superc m k ;
+        tl = Susp (fun () -> get_col (m+1) k)
+      }
+    in
+    let cols = get_col 0 in
+    let rec get_supcat n =
+      {
+        hd = cols n ;
+        tl = Susp (fun () -> get_supcat (n+1))
+      }
+    in
+    get_supcat 0
+    
+end
+                   
